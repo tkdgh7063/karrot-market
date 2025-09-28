fec6e23f0f08ee958398ea37693d3e2902f8c4da
tkdgh7063 <hoo412298@gmail.com>
2025-09-28 19:04:27 +0900

    Practice 7: List modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-gray-100 p-5 sm:bg-red-300 md:bg-yellow-300 lg:bg-green-300 xl:bg-cyan-300 2xl:bg-violet-300">
-      <div className="flex w-full max-w-screen-sm flex-col gap-2.5 rounded-3xl bg-white p-5 shadow-lg ring-2 ring-transparent transition-shadow *:outline-none has-[:invalid]:bg-red-100 has-[:invalid]:ring-red-400 md:flex-row">
-        <input
-          className="peer h-12 w-full rounded-full bg-gray-200 pl-4 ring-2 ring-transparent transition-shadow placeholder:drop-shadow valid:ring-green-500 invalid:ring-red-500 focus:ring-offset-2 invalid:focus:ring-red-500"
-          type="text"
-          required
-          placeholder="Email address"
-        />
-        <span className="ml-4 hidden font-medium text-red-400 peer-invalid:block">
-          Email is Required.
-        </span>
-        <button className="rounded-full bg-gradient-to-tr from-cyan-500 via-violet-400 to-purple-300 py-2 font-medium text-white transition-transform peer-invalid:bg-red-300 peer-invalid:bg-none active:scale-95 md:px-8">
-          Log In
-        </button>
+      <div className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl bg-white p-5 shadow-lg">
+        {["Alex", "Mike", "Alice", "Max"].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 rounded-t-xl border-b-2 p-2.5 pb-6 last:border-0 last:pb-0 odd:bg-gray-400"
+          >
+            <div className="size-10 rounded-full bg-cyan-500" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="flex size-6 items-center justify-center rounded-full bg-red-500 text-white">
+              <span className="text-base font-light">{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
