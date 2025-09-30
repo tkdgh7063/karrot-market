40b4b6385a6ce42e64c07a64adf8b5c4887c14de
tkdgh7063 <hoo412298@gmail.com>
2025-09-30 17:39:52 +0900

    feat(screen): Home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.className} mx-auto max-w-screen-sm bg-neutral-900 text-white antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-5">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="mb-3 ml-5 text-9xl">🥕</span>
+        <h1 className="text-4xl">Karrot</h1>
+        <h2 className="text-2xl">Welcome to Karrot Market</h2>
+      </div>
+      <div className="flex w-full flex-col items-center gap-1">
+        <Link
+          href="/create-account"
+          className="w-[80%] rounded-lg bg-orange-500 py-2.5 text-center text-lg text-white transition-colors hover:bg-orange-400"
+        >
+          Get Started
+        </Link>
+        <div className="flex gap-2">
+          <span>Already have an account?</span>
+          <Link href="/login" className="underline-offset-4 hover:underline">
+            Login
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
