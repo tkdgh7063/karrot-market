b83f67f5aec8ab4145c31fa8146c2540fae63ccb
tkdgh7063 <hoo412298@gmail.com>
2025-10-15 00:00:34 +0900

    feat(logout): logout in profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,36 @@
-export default function Profile() {
-  return <h1>Welcome to your profile</h1>;
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  } else {
+    notFound();
+  }
+}
+
+export default async function Profile() {
+  const logout = async () => {
+    "use server";
+    (await getSession()).destroy();
+    redirect("/");
+  };
+  const user = await getUser();
+  return (
+    <div>
+      <h1>Welcome {user?.username}!</h1>
+      <form action={logout}>
+        <button>Logout</button>
+      </form>
+    </div>
+  );
 }
