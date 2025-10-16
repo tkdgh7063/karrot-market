f5a1cdc7e054fc490f0da74c99efa52a5481370e
tkdgh7063 <hoo412298@gmail.com>
2025-10-16 19:05:59 +0900

    feat: add tab bar for authenticated users

---
+++ components/tab-bar.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import {
+  HomeIcon as SolidHomeIcon,
+  NewspaperIcon as SolidNewspaperIcon,
+  ChatBubbleLeftRightIcon as SolidChatIcon,
+  VideoCameraIcon as SolidCameraIcon,
+  UserCircleIcon as SolidUserIcon,
+} from "@heroicons/react/24/solid";
+import {
+  HomeIcon as OutlineHomeIcon,
+  NewspaperIcon as OutlineNewspaperIcon,
+  ChatBubbleLeftRightIcon as OutlineChatIcon,
+  VideoCameraIcon as OutlineCameraIcon,
+  UserCircleIcon as OutlineUserIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function TabBar() {
+  const pathname = usePathname();
+  return (
+    <div className="fixed bottom-0 mx-auto grid w-full max-w-screen-md grid-cols-5 border-t border-neutral-600 bg-neutral-900 px-2 py-3 *:text-white">
+      <Link href="/products" className="flex flex-col items-center gap-px">
+        {pathname === "/products" ? (
+          <SolidHomeIcon className="size-7" />
+        ) : (
+          <OutlineHomeIcon className="size-7" />
+        )}
+        <span>Home</span>
+      </Link>
+      <Link href="/life" className="flex flex-col items-center gap-px">
+        {pathname === "/life" ? (
+          <SolidNewspaperIcon className="size-7" />
+        ) : (
+          <OutlineNewspaperIcon className="size-7" />
+        )}
+        <span>동네생활</span>
+      </Link>
+      <Link href="/chat" className="flex flex-col items-center gap-px">
+        {pathname === "/chat" ? (
+          <SolidChatIcon className="size-7" />
+        ) : (
+          <OutlineChatIcon className="size-7" />
+        )}
+        <span>Chat</span>
+      </Link>
+      <Link href="/live" className="flex flex-col items-center gap-px">
+        {pathname === "/live" ? (
+          <SolidCameraIcon className="size-7" />
+        ) : (
+          <OutlineCameraIcon className="size-7" />
+        )}
+        <span>Shopping</span>
+      </Link>
+      <Link href="/profile" className="flex flex-col items-center gap-px">
+        {pathname === "/profile" ? (
+          <SolidUserIcon className="size-7" />
+        ) : (
+          <OutlineUserIcon className="size-7" />
+        )}
+        <span>My Profile</span>
+      </Link>
+    </div>
+  );
+}
