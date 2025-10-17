2ef3fa4f7f4ab695e59244205bcc23dec9d8ae07
tkdgh7063 <hoo412298@gmail.com>
2025-10-17 23:00:46 +0900

    feat(product-detail): complete product detail page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,31 @@
-async function getProduct() {
-  return new Promise((resolve) => setTimeout(resolve, 120000));
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { formatToWon } from "@/lib/utils";
+import { UserIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+async function getIsOwner(userId: number) {
+  const session = await getSession();
+  if (session.id) return Boolean(session.id === userId);
+  return false;
+}
+
+async function getProduct(id: number) {
+  const product = await db.product.findUnique({
+    where: { id },
+    include: {
+      user: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+
+  return product;
 }
 
 export default async function ProductDetail({
@@ -7,13 +33,63 @@ export default async function ProductDetail({
 }: {
   params: Promise<{ id: string }>;
 }) {
-  const { id } = await params;
-  const product = await getProduct();
+  const id = Number((await params).id);
+  if (isNaN(id)) {
+    return notFound();
+  }
+  const product = await getProduct(id);
+  if (!product) {
+    return notFound();
+  }
+
+  const isOwner = await getIsOwner(product.userId);
+  console.log(isOwner);
   return (
     <div>
-      <h1 className="text-xl text-white">
-        Product Detail of the product {id}!
-      </h1>
+      <div className="relative aspect-square">
+        <Image src={product.photo ?? ""} alt={product.title} fill />
+      </div>
+      <div className="flex items-center gap-3 border-b border-neutral-700 p-5">
+        <div className="size-10 rounded-full">
+          {product.user.avatar !== null ? (
+            <Image
+              src={product.user.avatar}
+              width={40}
+              height={40}
+              alt={product.user.username}
+            />
+          ) : (
+            <UserIcon />
+          )}
+        </div>
+        <div>
+          <h3>{product.user.username}</h3>
+        </div>
+      </div>
+      <div className="p-5">
+        <h1 className="text-2xl font-semibold">{product.title}</h1>
+        <p>{product.description}</p>
+      </div>
+      <div className="fixed bottom-0 left-0 flex w-full items-center justify-between bg-neutral-800 p-5 pb-10">
+        <span className="text-xl font-semibold">
+          {formatToWon(product.price)} Won
+        </span>
+        {isOwner ? (
+          <Link
+            className="rounded-md bg-red-500 px-4 py-2.5 font-semibold text-white"
+            href={``}
+          >
+            Delete Product
+          </Link>
+        ) : (
+          <Link
+            className="rounded-md bg-orange-500 px-4 py-2.5 font-semibold text-white"
+            href={``}
+          >
+            Chat with seller
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
