edc8c81cbc87f9a3f31affd950cc8ce5bef55c13
tkdgh7063 <hoo412298@gmail.com>
2025-10-18 03:21:50 +0900

    chore(next): configure remote image optimization

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -24,7 +24,6 @@ async function getProduct(id: number) {
       },
     },
   });
-
   return product;
 }
 
@@ -43,14 +42,13 @@ export default async function ProductDetail({
   }
 
   const isOwner = await getIsOwner(product.userId);
-  console.log(isOwner);
   return (
     <div>
       <div className="relative aspect-square">
         <Image src={product.photo ?? ""} alt={product.title} fill />
       </div>
       <div className="flex items-center gap-3 border-b border-neutral-700 p-5">
-        <div className="size-10 rounded-full">
+        <div className="size-10 overflow-hidden rounded-full">
           {product.user.avatar !== null ? (
             <Image
               src={product.user.avatar}
