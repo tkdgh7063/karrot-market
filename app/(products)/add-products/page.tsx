9598caa98b5cf4cd1beec0f323b0d23813b778ec
tkdgh7063 <hoo412298@gmail.com>
2025-11-13 17:01:04 +0900

    refactor: remove unnecessary div wrapper from page

--- app/(products)/add-products/page.tsx
+++ app/(products)/add-products/page.tsx
@@ -24,62 +24,60 @@ export default function NewProduct() {
     setPreview(url);
   };
   return (
-    <div className="">
-      <form action={action} className="flex flex-col gap-5 p-5">
-        <h1 className="text-2xl">New Product</h1>
-        <Input
-          name="title"
-          placeholder="Title"
-          type="text"
-          errors={state?.fieldErrors.title}
-          required
-        />
-        <Input
-          name="price"
-          placeholder="Price"
-          type="number"
-          min={0}
-          onChange={(e) => {
-            const value = Number(e.target.value);
-            if (value < 0) return;
-            e.target.value = value.toString();
-          }}
-          errors={state?.fieldErrors.price}
-          required
-        />
-        <Input
-          name="description"
-          placeholder="Description"
-          type="text"
-          errors={state?.fieldErrors.description}
-          required
-        />
-        <label
-          htmlFor="photo"
-          style={{ backgroundImage: `url(${preview})` }}
-          className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-neutral-400 bg-cover bg-center text-neutral-400"
-        >
-          {preview ? null : (
-            <>
-              <PhotoIcon className="h-32" />
-              <div className="text-lg font-semibold">
-                Upload a photo
-                {state?.fieldErrors.photo}
-              </div>
-            </>
-          )}
-        </label>
-        <input
-          onChange={onImageUpload}
-          type="file"
-          name="photo"
-          id="photo"
-          accept="image/*"
-          hidden
-          required
-        />
-        <Button text="Upload Product" />
-      </form>
-    </div>
+    <form action={action} className="flex flex-col gap-5 p-5">
+      <h1 className="text-2xl">New Product</h1>
+      <Input
+        name="title"
+        placeholder="Title"
+        type="text"
+        errors={state?.fieldErrors.title}
+        required
+      />
+      <Input
+        name="price"
+        placeholder="Price"
+        type="number"
+        min={0}
+        onChange={(e) => {
+          const value = Number(e.target.value);
+          if (value < 0) return;
+          e.target.value = value.toString();
+        }}
+        errors={state?.fieldErrors.price}
+        required
+      />
+      <Input
+        name="description"
+        placeholder="Description"
+        type="text"
+        errors={state?.fieldErrors.description}
+        required
+      />
+      <label
+        htmlFor="photo"
+        style={{ backgroundImage: `url(${preview})` }}
+        className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-neutral-400 bg-cover bg-center text-neutral-400"
+      >
+        {preview ? null : (
+          <>
+            <PhotoIcon className="h-32" />
+            <div className="text-lg font-semibold">
+              Upload a photo
+              {state?.fieldErrors.photo}
+            </div>
+          </>
+        )}
+      </label>
+      <input
+        onChange={onImageUpload}
+        type="file"
+        name="photo"
+        id="photo"
+        accept="image/*"
+        hidden
+        required
+      />
+      <Button text="Upload Product" />
+    </form>
   );
 }
