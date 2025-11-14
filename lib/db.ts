0fcccb4fd9d6dcb7dca5b5346707cb92d31b8a7b
tkdgh7063 <hoo412298@gmail.com>
2025-11-15 01:23:24 +0900

    refactor: remove test function from db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -2,18 +2,4 @@ import { PrismaClient } from "./generated/prisma";
 
 const db = new PrismaClient();
 
-async function test() {
-  const code = await db.sMSCode.create({
-    data: {
-      code: "1231212123",
-      user: {
-        connect: {
-          id: 1,
-        },
-      },
-    },
-  });
-  console.log(code);
-}
-
 export default db;
