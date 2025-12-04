5d13385e3f76ff078fcafab436752f77dd083a70
tkdgh7063 <hoo412298@gmail.com>
2025-12-04 18:11:31 +0900

    style: Add cursor-pointer to shared button for clearer hover interaction

--- components/button.tsx
+++ components/button.tsx
@@ -10,7 +10,7 @@ export default function Button({ text }: ButtonProps) {
   const { pending } = useFormStatus();
   return (
     <button
-      className="primary-btn h-10 w-full self-center font-semibold disabled:cursor-not-allowed disabled:bg-neutral-400 disabled:text-neutral-300 disabled:hover:bg-neutral-400"
+      className="primary-btn h-10 w-full self-center font-semibold hover:cursor-pointer disabled:cursor-not-allowed disabled:bg-neutral-400 disabled:text-neutral-300 disabled:hover:bg-neutral-400"
       disabled={pending}
     >
       {pending ? "Loading..." : text}
